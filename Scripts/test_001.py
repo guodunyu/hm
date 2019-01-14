24cb08c21566307a5dcf87ab16950b4e27537eca
guodunyu <1016912465@qq.com>
2019-01-14 11:02:25 +0800

    first commit

---
+++ Scripts/test_001.py
@@ -0,0 +1,13 @@
+class Test_001():
+    def test_001(self):
+        print("test001被执行")
+        assert 1
+    def test_002(self):
+        print("test002被执行")
+        assert 1
+    def test_003(self):
+        print("test003执行")
+        assert 1
+    def test_004(self):
+        print("test003执行")
+        assert 0
